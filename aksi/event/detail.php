ef90bd81bf3389abb2fc250d65001cd8ee357a79
Riyan Satria <halo@riyansatria.tk>
2018-08-16 11:27:56 +0700

    Updatean

--- aksi/event/detail.php
+++ aksi/event/detail.php
@@ -16,6 +16,7 @@ $title = $event->info($idevent, "title");
 $covers = $event->info($idevent, "covers");
 $availSeat = $event->info($idevent, "availableseat");
 $price = $event->info($idevent, "price");
+$hint = $event->info($idevent, "hint");
 
 $totWA = $track->tot($idevent, "1");
 $totCall = $track->tot($idevent, "2");
@@ -32,7 +33,7 @@ $priceLaku = $laku * $price;
     <img src="../aset/gbr/<?php echo $covers; ?>" id="cover">
 </div>
 <div class="ke-kiri" id="detailnya">
-    <li><div id="icon"><i class="fa fa-eye"></i></div> 2014</li>
+    <li><div id="icon"><i class="fa fa-eye"></i></div> <?php echo $hint; ?> x</li>
     <li><div id="icon"><i class="fa fa-phone"></i></div> <?php echo $totCall; ?>x</li>
     <li><div id="icon"><i class="fa fa-whatsapp"></i></div> <?php echo $totWA; ?>x</li>
     <li><div id="icon"><i class="fa fa-money"></i></div> <?php echo toIdr($priceLaku); ?> ( <?php echo $laku; ?> seat )</li>
