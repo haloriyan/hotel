e1a7b4324061fb33c4e6e71dcadad814cbf79fa2
Riyan Satria <riyan.satria.backup@gmail.com>
2018-10-08 14:12:25 +0700

    sliderRange

--- pages/tes.php
+++ pages/tes.php
@@ -1,18 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale = 1">
+	<title>an</title>
+	<link href="aset/rangeSlider/css/ion.rangeSlider.css" rel="stylesheet">
+	<link href="aset/rangeSlider/css/ion.rangeSlider.skinFlat.css" rel="stylesheet">
+	<link href="aset/rangeSlider/css/normalize.css" rel="stylesheet">
+</head>
+<body style="text-align: center;">
 
-$namaPertama = 'Riyan';
-$namaKedua = 'Satria';
-$kolom = [$namaPertama,$namaKedua];
+<div style="width: 80%;display: inline-block;">
+	<input id="myRange">
+</div>
 
-$valPertama = 'Adi';
-$valKedua = 'Tama';
-$value = [$valPertama,$valKedua];
+<script src="aset/js/jquery-3.1.1.js"></script>
+<script src="aset/rangeSlider/js/ion-rangeSlider/ion.rangeSlider.min.js"></script>
+<script>
+	$("#myRange").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 100000,
+		from: 10000,
+		to: 15000,
+	    grid: false
+	})
+</script>
 
-function edit($id, $kolom, $value) {
-	$query = "UPDATE event SET ".$kolom." = '".$value."' WHERE idevent = '".$id."' <br />";
-	return $query;
-}
-
-for ($i=0; $i < count($kolom); $i++) { 
-	echo edit('123', $kolom[$i], $value[$i]);
-}
+</body>
+</html>
