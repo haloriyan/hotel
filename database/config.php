d53ec802673f4c5df16d7c5f1556185e7e267dd4
Riyan Satria <halo@riyansatria.tk>
2018-11-12 15:42:11 +0700

    Updatean

--- database/config.php
+++ database/config.php
@@ -6,4 +6,9 @@ $dbUsername = "root";
 $dbPassword = "";
 $dbName 	= "hotel";
 
+// $dbHost 	= "spectre.beon.co.id";
+// $dbUsername = "imajingg_test";
+// $dbPassword = "inikatasandi2908;";
+// $dbName 	= "imajingg_testDb";
+
 ?>
