87808709b928199daef59795d085c5cc5f5e73d8
Riyan Satria <halo@riyansatria.tk>
2018-11-17 21:49:48 +0700

    Updatean

--- database/config.php
+++ database/config.php
@@ -1,14 +1,14 @@
 <?php
 global $dbHost,$dbUsername,$dbPassword,$dbName;
 
-$dbHost 	= "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName 	= "hotel";
+// $dbHost 	= "localhost";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName 	= "hotel";
 
-// $dbHost 	= "spectre.beon.co.id";
-// $dbUsername = "imajingg_test";
-// $dbPassword = "inikatasandi2908;";
-// $dbName 	= "imajingg_testDb";
+$dbHost 	= "spectre.beon.co.id";
+$dbUsername = "imajingg_test";
+$dbPassword = "inikatasandi2908;";
+$dbName 	= "imajingg_testDb";
 
 ?>
